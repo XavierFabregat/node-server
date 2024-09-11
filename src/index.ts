dd424b6e950676b3e49acd9daf2d6f97423de7bb
XavierFabregat <xavi.fabregat.pous@gmail.com>
2024-09-11 07:30:00 +0200

    remove dist from git

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello');
+import express from 'express';
+import './loadEnv';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
